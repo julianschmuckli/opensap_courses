3dc7ee3c0de97142b911aafea60d20ce9fb9ec80
Julian Schmuckli <julian@schmuckli.net>
2019-09-05 11:00:25 +0000

    Added process after Week 3

--- Build_Mobile_Applications_with_SAP_Cloud_Platform_Mobile_Services/CanteenService/srv/src/main/java/com/sap/canteen/handler/BookLunchHandler.java
+++ Build_Mobile_Applications_with_SAP_Cloud_Platform_Mobile_Services/CanteenService/srv/src/main/java/com/sap/canteen/handler/BookLunchHandler.java
@@ -2,11 +2,23 @@ package com.sap.canteen.handler;
 
 import com.sap.canteen.proxy.Booking;
 import com.sap.canteen.proxy.DishMenu;
-import com.sap.cloud.server.odata.*;
+import com.sap.cloud.server.odata.BooleanValue;
+import com.sap.cloud.server.odata.DataMethod;
+import com.sap.cloud.server.odata.DataValue;
+import com.sap.cloud.server.odata.LongValue;
+import com.sap.cloud.server.odata.ParameterList;
+import com.sap.cloud.server.odata.StringValue;
+import com.sap.cloud.server.odata.http.HttpRequest;
+import com.sap.cloud.server.odata.json.JsonObject;
+import com.sap.cloud.server.odata.json.JsonValue;
 
 public class BookLunchHandler extends com.sap.cloud.server.odata.DefaultMethodHandler {
     private com.sap.canteen.MainServlet servlet;
     private com.sap.canteen.proxy.CanteenService service;
+    
+    private static String BASEURL = "https://mobile-service-c2g.cfapps.eu10.hana.ondemand.com/d4a352bb-3fa8-47e0-add8-19a63107275b/v1/register/templated";
+    private static String APIKEY = "d87ba57574ea091dcf17d52a1b13d0edc1f3e024a6a92a6543ae7e27e3b5cb13";
+    private static String ALIAS = "canteen";
 
     public BookLunchHandler(com.sap.canteen.MainServlet servlet, com.sap.canteen.proxy.CanteenService service) {
         super(servlet, service);
@@ -34,7 +46,39 @@ public class BookLunchHandler extends com.sap.cloud.server.odata.DefaultMethodHa
 		booking.setStatus("booked");
 		service.createEntity(booking); //this acutally touches the DB
 		
-		//Response handling         
-		return true; 
+		//Send confirmation card to user
+	    /*
+	    {
+	        "method": "REGISTER",
+	        "username": "mobile4opensap@gmail.com",
+	        "match" : "BookingSet(1)"
+	    }
+	    }*/
+		
+		JsonObject requestBody = new JsonObject();
+		requestBody.set("method", JsonValue.fromString("REGISTER"));
+		requestBody.set("username", JsonValue.fromString(UserID));
+		requestBody.set("match", JsonValue.fromString("BookingSet("+booking.getBookingID()+")"));
+		
+		HttpRequest request = new HttpRequest();
+		request.open("POST", BookLunchHandler.BASEURL);
+		
+		request.setRequestHeader("Content-Type", "application/json");
+		request.setUsername(BookLunchHandler.ALIAS);
+		request.setPassword(BookLunchHandler.APIKEY);
+		request.setRequestText(requestBody.toString());
+		
+		request.send();
+		
+		int status = request.getStatus();
+		
+	    request.close();
+	
+	    //Response handling 
+	    if ((status > 200) && (status <= 210)) {
+	        return true; 
+	    }   
+	    //Todo: Add proper error handling
+		return false;
     }
 }
